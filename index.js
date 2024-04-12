9ecc50dcbd338a01c6f56ea95787534f525dbda1
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-04-12 20:24:37 +0300

    Practice task resolve

--- index.js
+++ index.js
@@ -5,21 +5,39 @@ const PORT = 5000;
 
 const requestListener = async (request, response) => {
     const { url } = request;
-    
-    if(url === './index.html') { // http://localhost:5000/index.html
+
+    if(url === '/index.html') { // http://localhost:5000/index.html
         try {
-        const data = await fs.readFile('./views/index.html, utf-8')
-        response.statusCode = 200;
-        response.end(data);
+            const data = await fs.readFile('./views/index.html', 'utf-8');
+            response.statusCode = 200;
+            response.end(data);
         } catch (error) {
-            response.statusCode = 400;
-            response.end(data);      
+            response.statusCode = 404;
+            response.end();
         }
-    } else {
+    } else if (url === '/style.css') {
+        try {
+            const data = await fs.readFile('./views/style.css', 'utf-8');
+            response.statusCode = 200;
+            response.end(data);
+        } catch (error) {
+            response.statusCode = 404;
+            response.end();
+        }
+    } 
+    else {
         response.statusCode = 404;
         response.end();
     }
-}                         
+}
 const server = http.createServer(requestListener);
 
 server.listen(PORT);
+
+/*
+
+Задача: коли приходить запит на url
+/style.css
+Ми читаємо файл style.css і відпарвляємо його у відповідь
+
+*/
